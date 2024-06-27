3cacebf1a807bb28e7630f6cb5664f9ed1dae6c5
Olav Rønnestad Birkeland <6450056+o-l-a-v@users.noreply.github.com>
2024-06-27 20:28:59 +0200

    Removed ts-nocheck

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -1,5 +1,3 @@
-// @ts-nocheck
-
 import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
