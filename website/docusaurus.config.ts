631ca1be38647fb4b2fa2082470300c47c681781
Olav Rønnestad Birkeland <6450056+o-l-a-v@users.noreply.github.com>
2024-06-27 21:22:09 +0200

    Fix previous autoformat goof

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -75,7 +75,8 @@ const config: Config = {
         {
           to: '/blog',
           label: 'Blog',
-          position: 'left'},
+          position: 'left'
+        },
         {
           href: 'https://github.com/o-l-a-v/o-l-a-v.github.io',
           position: 'right',
